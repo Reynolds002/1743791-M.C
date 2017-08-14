2f590b8d6c3c532ec1eb94a3a5f8845113f3a197
Reynolds002 <30852494+Reynolds002@users.noreply.github.com>
2017-08-13 23:24:17 -0500

    Add files via upload

---
+++ locuras python.py
@@ -0,0 +1,143 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> num=10
+>>> num2=35
+>>> suma= num + num2
+>>> print "suma"
+SyntaxError: Missing parentheses in call to 'print'
+>>> print "hola2"
+SyntaxError: Missing parentheses in call to 'print'
+>>> print("suma")
+suma
+>>> cls
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    cls
+NameError: name 'cls' is not defined
+>>> num=26:
+	
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+>>> 
+>>> 
+>>> num1=10
+>>> num2=36
+>>> suma= num1+num2
+>>> print"suma"
+SyntaxError: invalid syntax
+>>> print suma
+SyntaxError: Missing parentheses in call to 'print'
+>>> print(suma)
+46
+>>> print(AHUEVO JAJAJAAJ QUE LA MADRE suma)
+SyntaxError: invalid syntax
+>>> print(AHUEVO JAJAJAAJ QUE LA MADRE)
+SyntaxError: invalid syntax
+>>> print(lito)
+Traceback (most recent call last):
+  File "<pyshell#39>", line 1, in <module>
+    print(lito)
+NameError: name 'lito' is not defined
+>>> print "listo"
+SyntaxError: Missing parentheses in call to 'print'
+>>> print(hola)
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    print(hola)
+NameError: name 'hola' is not defined
+>>> a= listo alex
+SyntaxError: invalid syntax
+>>> a=listo
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    a=listo
+NameError: name 'listo' is not defined
+>>> num=alex
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    num=alex
+NameError: name 'alex' is not defined
+>>> a=(a,l,e,x)
+Traceback (most recent call last):
+  File "<pyshell#45>", line 1, in <module>
+    a=(a,l,e,x)
+NameError: name 'a' is not defined
+>>> cls
+Traceback (most recent call last):
+  File "<pyshell#46>", line 1, in <module>
+    cls
+NameError: name 'cls' is not defined
+>>> print(suma)
+46
+>>> alex=listo
+Traceback (most recent call last):
+  File "<pyshell#48>", line 1, in <module>
+    alex=listo
+NameError: name 'listo' is not defined
+>>> alex="listo"
+>>> a="alex"
+>>> b="ahuevo"
+>>> c="ya salio creo"
+>>> print(listo,a,b,c)
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    print(listo,a,b,c)
+NameError: name 'listo' is not defined
+>>> print(listo)
+Traceback (most recent call last):
+  File "<pyshell#54>", line 1, in <module>
+    print(listo)
+NameError: name 'listo' is not defined
+>>> print(alex,a,b,c)
+listo alex ahuevo ya salio creo
+>>> for:
+	
+SyntaxError: invalid syntax
+>>> for i=0 :
+	
+SyntaxError: invalid syntax
+>>> for( i=0) :
+	
+SyntaxError: invalid syntax
+>>> h=Nombre profe no le hayo jajaja
+SyntaxError: invalid syntax
+>>> h="Nombre profe no le hayo jajaja"
+>>> print(h)
+Nombre profe no le hayo jajaja
+>>> 
